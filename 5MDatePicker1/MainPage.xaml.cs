fd6bc5b3eea8371250fa36a784aaba82283f3adc
Mirosław Madej <m.madej@zsp3zamosc.pl>
2024-09-10 11:39:40 +0200

    Dodaj pliki projektów.

---
+++ 5MDatePicker1/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace _5MDatePicker1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            dpDatePickerRez2.Date = DateTime.Now.AddDays(1);
+            dpDatePickerRez2.MinimumDate = DateTime.Now.AddDays(1);
+            dpDatePickerRez1.MinimumDate = DateTime.Now;
+        }
+
+        private void onDatePicker1Selected(object sender, EventArgs e)
+        {
+            lblData.Text = dpDatePicker1.Date.ToString("yyyy-MM-dd");
+        }
+        private void btnUstawClicked(object sender, EventArgs e)
+        {
+            dpDatePicker2.Date = new DateTime(2024, 05, 15);
+        }
+
+        private void dpRezerwackaSelected(object sender, EventArgs e)
+        {
+            if (dpDatePickerRez1.Date > dpDatePickerRez2.Date)
+                lblDni.Text = "zły wybór";
+            else
+                lblDni.Text = "dobry wybór";
+        }
+
+    }
+
+}
